382890d7e5ff0627088d68cc4d62934dd047929d
Harish Tayyar Madabushi <harish.tmh@gmail.com>
2021-08-18 23:27:34 +0100

    updated script name

--- Dataset/Task2/SubtaskA-Pre_Train/Evaluation/eval.sh
+++ Dataset/Task2/SubtaskA-Pre_Train/Evaluation/eval.sh
@@ -1,6 +1,5 @@
 set -e
 
-
 ## Change if you've used your own model to create evaluation data. 
 echo "Change Language to PT if running PT!"
 
@@ -16,26 +15,26 @@ fi
 
 ## Not making use of special tokens.
 echo "bert-base-cased-tokenised, evalData"
-python task2Evaluation.py \
+python task2SubtaskAEvaluation.py   \
        "output-no-git/$model/STModels/bert-base-cased-tokenised/v3-s2/" \
        "${basefolder}/evalData/"
 
 echo "****************************************************************************************************"
 ## Make use of special tokens.
 echo "bert-base-cased-tokenised, evalDataAllTokenised"
-python task2Evaluation.py \
+python task2SubtaskAEvaluation.py   \
        "output-no-git/$model/STModels/bert-base-cased-tokenised/v3-s2/" \
        "${basefolder}/evalDataAllTokenised/"
     
 echo "****************************************************************************************************"
 echo "pt-e5-all evalDataAllTokenised"
-python task2Evaluation.py  \
+python task2SubtaskAEvaluation.py    \
        "output-no-git/$model/STModels/pt-e5-all/v3-s5/"  \
        "${basefolder}/evalDataAllTokenised/"
 
 echo "****************************************************************************************************"
 echo "pt-e5-select evalDataSelectTokenised"
-python task2Evaluation.py  \
+python task2SubtaskAEvaluation.py    \
        "output-no-git/$model/STModels/pt-e5-select/v3-s5/" \
        "${basefolder}/evalDataSelectTokenised/"
 
